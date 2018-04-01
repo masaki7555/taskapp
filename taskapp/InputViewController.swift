719753d533932eefc69b0077fcbf7ef47f19cf7c
masaki7555 <golfsakurasaku@gmail.com>
2018-04-01 10:10:51 +0900

    Initial Commit

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,92 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 宇都宮 正暉 on 2018/03/23.
+//  Copyright © 2018年 masaki.utsunomiya. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import UserNotifications
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextfield: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var task: Task!
+    let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        titleTextfield.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextfield.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        setNotification(task: task)
+        
+        super.viewWillDisappear(animated)
+    }
+    // タスクのローカル通知を登録する
+    func setNotification(task: Task) {
+        let content = UNMutableNotificationContent()
+        // タイトルと内容を設定(中身がない場合メッセージ無しで音だけの通知になるので「(xxなし)」を表示する)
+        if task.title == "" {
+            content.title = "(タイトルなし)"
+        } else {
+            content.title = task.title
+        }
+        if task.contents == "" {
+            content.body = "(内容なし)"
+        } else {
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default()
+        
+        // ローカル通知が発動するtrigger（日付マッチ）を作成
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateComponents, repeats: false)
+        
+        // identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存）
+        let request = UNNotificationRequest.init(identifier: String(task.id), content: content, trigger: trigger)
+        
+        // ローカル通知を登録
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error ?? "ローカル通知登録 OK")  // error が nil ならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+        }
+
+        // 未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
+    }
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+}
---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 宇都宮 正暉 on 2018/03/23.
+//  Copyright © 2018年 masaki.utsunomiya. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
